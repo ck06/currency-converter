b3df8fbe8caaf4b877acd2ad4beac856a7a2d6ed
ck06 <35151010+ck06@users.noreply.github.com>
2025-02-02 11:59:08 +0100

    add real id values to fixture data
    This prevents data corruption in the dev/test environments if you run fixtures and then subsequently run the importer without truncating your table first.

--- src/DataFixtures/CurrencyFixture.php
+++ src/DataFixtures/CurrencyFixture.php
@@ -22,25 +22,24 @@ class CurrencyFixture extends Fixture
 
     private function createCurrencies(): void
     {
-        $data = self::currencyData();
-        $max = count($data);
-        for ($i = 1; $i <= $max; $i++) {
-            $current = $data[$i - 1];
+        foreach (self::currencyData() as $current) {
             $currency = $this->createCurrency(
-                id: $i,
+                id: $current['id'],
                 code: $current['code'],
                 name: $current['name'],
-                rate: $current['rate'],
+                rate: $curren        $data = self::currencyData();
+        $max = count($data);
+t['rate'],
             );
 
             $this->manager->persist($currency);
         }
     }
 
-    private function createCurrency(int $id, string $code, string $name, float $rate): Currency
+    private function createCurrency(string $id, string $code, string $name, float $rate): Currency
     {
         return (new Currency())
-            ->setNumericCode((string)$id)
+            ->setNumericCode($id)
             ->setAlphaCode($code)
             ->setCode($code)
             ->setName($name)
@@ -53,31 +52,37 @@ class CurrencyFixture extends Fixture
     {
         return [
             [
+                'id' => '978',
                 'code' => 'EUR',
                 'name' => 'European Euro',
                 'rate' => 1.0,
             ],
             [
+                'id' => '840',
                 'code' => 'USD',
                 'name' => 'American Dollar',
                 'rate' => 2.0,
             ],
             [
+                'id' => '124',
                 'code' => 'CAD',
                 'name' => 'Canadian Dollar',
                 'rate' => 4.5,
             ],
             [
+                'id' => '392',
                 'code' => 'JPY',
                 'name' => 'Japanese Yen',
                 'rate' => 100,
             ],
             [
+                'id' => '410',
                 'code' => 'KRW',
                 'name' => 'Korean Won',
                 'rate' => 1000,
             ],
             [
+                'id' => '608',
                 'code' => 'PHP',
                 'name' => 'PHP: Hypertext Preprocessor',
                 'rate' => 0.333333333333,
