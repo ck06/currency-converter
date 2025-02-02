2b718988ab5f4d10535a17f2844db7fb071dfeb3
ck06 <>
2025-02-02 11:25:50 +0100

    Fix styling in files to match PSR1/2
    I normally have this done automatically but my IDE isn't set up for it. and I don't want to spend too much time configuring a temporary install

--- src/Command/CurrencyImportTask.php
+++ src/Command/CurrencyImportTask.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Command;
 
 use App\Service\CurrencyImporter;
-use http\Client;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
