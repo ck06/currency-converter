777527d195f6ed94f62d346c509eeebeaee2a58c
ck06 <>
2025-02-02 10:13:59 +0100

    Create import command and a template for the importer

---
+++ src/Service/CurrencyImporter.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+class CurrencyImporter
+{
+    public function importAll(string $data): void
+    {
+    }
+
+    public function importOne(string $data, string $target): void
+    {
+    }
+
+    public function createDefault(string $data, string $codeForDefault): void
+    {
+    }
+}
