2a5c35d26b307135fef5c135b100c59788afed4f
ck06 <35151010+ck06@users.noreply.github.com>
2025-02-02 17:17:53 +0100

    Fixture to add whitelists to our dev users

---
+++ src/DataFixtures/WhitelistFixture.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Currency;
+use App\Entity\User;
+use App\Entity\Whitelist;
+use App\Service\Inverter;
+use DateTimeImmutable;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class WhitelistFixture extends Fixture implements DependentFixtureInterface
+{
+    private ObjectManager $manager;
+
+    public function getDependencies(): array
+    {
+        return [
+            UserFixture::class,
+        ];
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->manager = $manager;
+
+        foreach (['user', 'admin'] as $username) {
+            $user = $this->getReference(UserFixture::REFERENCE_PREFIX.$username, User::class);
+            $this->createWhitelist($user);
+            $this->manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+
+    private function createWhitelist(User $user): void
+    {
+        $addresses = ['127.0.0.1'];
+        foreach ($addresses as $address) {
+            $whitelist = (new Whitelist())->setUser($user)->setAddress($address);
+            $this->manager->persist($whitelist);
+        }
+    }
+}
