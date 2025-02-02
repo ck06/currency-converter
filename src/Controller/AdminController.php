729b38cf7c55afb7a0b5f21b2f35a0fb092bfcdb
ck06 <>
2025-02-02 08:19:42 +0100

    Create empty controller templates

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'admin')]
+    public function load(Request $request): Response
+    {
+        return new JsonResponse();
+    }
+}
